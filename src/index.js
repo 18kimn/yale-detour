b29f002aa4a7a99d48bc2761e2818ef75c898b6d
Nathan <nathanckim18@gmail.com>
2021-09-22 20:37:54 -0400

    updated font to zilla slab

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import WebFont from 'webfontloader'
 import Home from './components/Home'
 
-WebFont.load({
-  google: {
-    families: ['Roboto:300,700', 'sans-serif'],
-  },
-})
 
 ReactDOM.render(<Home />, document.getElementById('root'))
