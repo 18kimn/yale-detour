447c2f6aec5e12775b9274e9c5a1570516703003
Nathan Kim <nathanckim18@gmail.com>
2022-01-06 14:55:08 -0500

    halfway through migration

--- config-overrides.js
+++
@@ -1,3 +0,0 @@
-const {override, addBundleVisualizer} = require('customize-cra')
-
-module.exports = override(addBundleVisualizer({openAnalyzer: false}))
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Home from './components/Home'
-
-
-ReactDOM.render(<Home />, document.getElementById('root'))
